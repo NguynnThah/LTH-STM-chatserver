7e6bda9191ec8f6648e5866dfc6aa3219fc02a4a
NguynnThah <ngvlthanh.1825@gmail.com>
2025-11-15 15:34:44 +0700

    final

--- src/StressTest.hs
+++ src/StressTest.hs
@@ -1,5 +1,6 @@
--- Một client mô phỏng để "tấn công" (stress test) STM Chat Server
--- Biên dịch: ghc --make src/StressTest.hs -threaded -o StressTest.exe -package network -package random -package base
+-- StressTest.hs 
+-- Dùng để demo giao diện Python chạy vù vù
+-- Compile: ghc --make src/StressTest.hs -threaded -o StressTest.exe -package network -package random -package base
 
 import Network.Socket
 import System.IO
@@ -9,30 +10,31 @@ import System.Random (randomRIO)
 import Text.Printf (printf)
 
 ----------------------------------------------------
--- CẤU HÌNH TEST
+-- CẤU HÌNH
 ----------------------------------------------------
 numClients :: Int
 numClients = 50 
 
 testDurationSec :: Int
-testDurationSec = 20 
+testDurationSec = 30 
 
 serverHost :: String
-serverHost = "127.0.0.1"
+serverHost = "192.168.191.38"     
 
 serverPort :: String
 serverPort = "8080" 
 
 chaosRoom :: String
-chaosRoom = "#LTH-ChatServer-STM" 
+chaosRoom = "#chaos" 
 
 ----------------------------------------------------
--- HÀM MAIN
+-- MAIN
 ----------------------------------------------------
 main :: IO ()
 main = withSocketsDo $ do
-  hSetEncoding stdout utf8 
-  printf "--- Bat dau kich ban 'Hon loan' ---\n"
+  hSetEncoding stdout utf8 -- Sửa lỗi font tiếng Việt trên Windows
+  
+  printf "--- Bat dau che do SPAM demo ---\n"
   printf "--- Tao %d client ao... \n" numClients
   
   replicateM_ numClients $ do
@@ -43,56 +45,44 @@ main = withSocketsDo $ do
   printf "--- %d client dang tan cong server trong %d giay... ---\n" numClients testDurationSec
   threadDelay (testDurationSec * 1000000)
   
-  printf "--- Kich ban 'Hon loan' ket thuc. ---\n"
+  printf "--- Ket thuc kich ban. ---\n"
 
 ----------------------------------------------------
--- HÀM CLIENT CHÍNH
+-- CLIENT LOGIC
 ----------------------------------------------------
 runClient :: Int -> IO ()
 runClient clientId = do
-  (handle, username) <- connectToServer ("user" ++ show clientId)
+  (handle, username) <- connectToServer ("bot" ++ show clientId)
   
   hPutStrLn handle $ "LOGIN " ++ username
   hPutStrLn handle $ "JOIN " ++ chaosRoom
   hFlush handle
   
+  -- Lắng nghe 
   forkIO $ listenToServer handle
   
-  chaosLoop handle username
+  -- Bắt đầu Spam
+  spamLoop handle username
 
+-- Hàm lắng nghe đơn giản
 listenToServer :: Handle -> IO ()
 listenToServer handle = forever $ do
   _ <- hGetLine handle 
   return ()
 
-chaosLoop :: Handle -> String -> IO ()
-chaosLoop handle username = forever $ do
-  actionCode <- randomRIO (1, 10) :: IO Int
+-- VÒNG LẶP SPAM 
+spamLoop :: Handle -> String -> IO ()
+spamLoop handle username = forever $ do
+  let msg = "MSG " ++ chaosRoom ++ " " ++ username ++ " : spam tin nhan demo GUI!!!"
   
-  let actionMessage = case actionCode of
-        1 -> Just $ "PART " ++ chaosRoom
-        2 -> Just $ "PART " ++ chaosRoom
-        3 -> Just $ "PART " ++ chaosRoom
-        4 -> Just $ "PART " ++ chaosRoom
-        
-        5 -> Just $ "JOIN " ++ chaosRoom
-        6 -> Just $ "JOIN " ++ chaosRoom
-        7 -> Just $ "JOIN " ++ chaosRoom
-        8 -> Just $ "JOIN " ++ chaosRoom
-        
-        _ -> Just $ "MSG " ++ chaosRoom ++ " " ++ username ++ " dang spam"
+  hPutStrLn handle msg
+  hFlush handle
   
-  case actionMessage of
-    Just msg -> do
-      hPutStrLn handle msg
-      hFlush handle
-    Nothing -> return ()
-  
-  delay <- randomRIO (500000, 2000000) 
-  threadDelay delay
+  -- Tốc độ spam: 0.2 giây / 1 tin 
+  threadDelay 200000
 
 ----------------------------------------------------
--- HÀM HELPER
+-- KẾT NỐI
 ----------------------------------------------------
 connectToServer :: String -> IO (Handle, String)
 connectToServer username = do
